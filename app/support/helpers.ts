458c61a1110593187dcf2ce90e1280795d44eeee
Abdulazeez Shittu <41093124+abdulazeez001@users.noreply.github.com>
2024-06-07 09:57:17 +0100

    Update helpers.ts

--- app/support/helpers.ts
+++ app/support/helpers.ts
@@ -1,5 +1,7 @@
 export const removeProtocol = (url: string) => {
   if (!url) return null
   const extractUrlDetail = new URL(url)
-  return `${extractUrlDetail.hostname}${extractUrlDetail.pathname}`
+  return extractUrlDetail.search
+    ? `${extractUrlDetail.hostname}${extractUrlDetail.pathname}${extractUrlDetail.search}`
+    : `${extractUrlDetail.hostname}${extractUrlDetail.pathname}`
 }
